67c3d4b2b4ca5834664d28144d51df5989b16068
Sam <samjulian6@gmail.com>
2022-10-20 15:02:43 -0700

    Implement functionality to add new properties to app through query

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -162,9 +162,15 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const { owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code } = property;
+  const queryParams = [owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code];
+  const queryString = `
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+  `;
+  return pool.query(queryString, queryParams)
+  .then(result => result.rows[0])
+  .catch(err => console.log(err.message));
 }
 exports.addProperty = addProperty;
